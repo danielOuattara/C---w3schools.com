73b8e85143ccc5e521ba6fe8ce83a567c1eed465
danielOuattara <daniel.ouattara@gmx.com>
2023-01-27 23:39:15 +0100

    ouput

---
+++ 01_Cpp_Tutorial/05-output/05_Cpp_Output.cpp
@@ -0,0 +1,32 @@
+/*---------------------------
+Daniel.B OUATTARA
+C++ Tutorial  w3schools.com/cpp/
+daniel.ouattara[@]gmxDOTcom
+16 decembre 2020
+---------------------------*/
+
+/*
+
+16 decembre 2020
+
+C++ Output (Print Text)
+=======================
+
+The 'cout' object, together with the '<<' operator, is used
+to output values/print text:
+
+You can add as many cout objects as you want.
+However, note that it does not insert a new line
+at the end of the output:
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << '\n';
+    cout << "I am learning C++";
+
+    return 0;
+}
