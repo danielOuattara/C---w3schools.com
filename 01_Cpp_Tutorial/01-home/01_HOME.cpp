a5abb5dd8cd6f66ed52b817929cf65abaed4a907
danielOuattara <daniel.ouattara@gmx.com>
2023-01-27 23:38:31 +0100

    home

---
+++ 01_Cpp_Tutorial/01-home/01_HOME.cpp
@@ -0,0 +1,20 @@
+/*---------------------------
+Daniel.B OUATTARA
+C++ Tutorial w3schools.com/cpp/
+daniel.ouattara[@]gmxDOTcom
+16 decembre 2020
+---------------------------*/
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello World !" << endl;
+    return 0 ;
+}
+
+/*
+We recommend reading this tutorial, in the sequence listed in the left menu.
+C++ is an object oriented language and some concepts may be new. 
+*/
