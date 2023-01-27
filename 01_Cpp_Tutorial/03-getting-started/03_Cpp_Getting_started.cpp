2532b4ea09fb92a07634da911e963fbe4d699f5f
danielOuattara <daniel.ouattara@gmx.com>
2023-01-27 23:38:56 +0100

    getting started

---
+++ 01_Cpp_Tutorial/03-getting-started/03_Cpp_Getting_started.cpp
@@ -0,0 +1,26 @@
+/*---------------------------
+Daniel.B OUATTARA
+C++ Tutorial  w3schools.com/cpp/
+daniel.ouattara[@]gmxDOTcom
+16 decembre 2020
+---------------------------*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World Again !\n";
+    return 0;
+}
+
+/* C++ Get Started
+===================
+
+To start using C++, you need two things:
+
+>A text editor, like Notepad, to write C++ code
+>A compiler, like GCC, to translate the C++ code into
+   a language that the computer will understand
+
+There are many text editors and compilers to choose from. */
