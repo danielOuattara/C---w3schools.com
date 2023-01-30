4127da770e891b5cb5ff8b300a0b704634ae77a5
danielOuattara <daniel.ouattara@gmx.com>
2023-01-30 12:55:23 +0100

    comments

---
+++ A_Cpp_Tutorial/06-comments/comments.cpp
@@ -0,0 +1,13 @@
+/*---------------------------
+Daniel.B OUATTARA
+C++ Tutorial  w3schools.com/cpp/
+daniel.ouattara[@]gmxDOTcom
+16 decembre 2020
+---------------------------*/
+
+// This is a single line comment
+
+/*
+This is a
+multi-line comment
+*/
