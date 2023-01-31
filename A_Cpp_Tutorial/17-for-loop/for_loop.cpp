2e51d2f2aa370aed8f167a29d4da361befb3ce6b
danielOuattara <daniel.ouattara@gmx.com>
2023-01-31 20:03:11 +0100

    for loop

---
+++ A_Cpp_Tutorial/17-for-loop/for_loop.cpp
@@ -0,0 +1,32 @@
+/*---------------------------
+Daniel.B OUATTARA
+C++ Tutorial  w3schools.com/cpp/
+daniel.ouattara[@]gmxDOTcom
+02 février 2021
+---------------------------*/
+
+/*C++ For...
+============== */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "i = " << i << endl;
+    }
+
+    cout << "--------------------" << endl;
+
+    //-----------------------------
+    for (int j = 0; j < 15; j += 4)
+    {
+        cout << "j = " << j << endl;
+    }
+}
