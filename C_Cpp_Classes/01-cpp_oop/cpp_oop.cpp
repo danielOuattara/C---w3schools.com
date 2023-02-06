7f30c3d3675f101de50c5e954cdc8fad50fbcd64
danielOuattara <daniel.ouattara@gmx.com>
2023-02-06 23:07:08 +0100

    cpp OOP

---
+++ C_Cpp_Classes/01-cpp_oop/cpp_oop.cpp
@@ -0,0 +1,71 @@
+/*---------------------------
+Daniel.B OUATTARA
+C++ Tutorial  w3schools.com/cpp/
+daniel.ouattara[@]gmxDOTcom
+05 février 2021
+---------------------------*/
+
+/* C++ OOP
+=========== */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+/* C++ What is OOP ?
+=======================
+
+OOP stands for Object-Oriented Programming.
+
+Procedural Programming is about writing procedures or functions
+that perform operations on the data.
+
+Object-Oriented Programming is about creating objects that
+contain both data and functions.
+
+Object-oriented programming has several advantages over procedural
+programming:
+
+> OOP is faster and easier to execute
+
+> OOP provides a clear structure for the programs
+
+> OOP helps to keep the C++ code DRY "Don't Repeat Yourself", and makes
+  the code easier to maintain, modify and debug
+
+> OOP makes it possible to create full reusable applications with less
+  code and shorter development time
+
+  The "Don't Repeat Yourself" (DRY) principle is about reducing the
+  repetition of code. You should extract out the codes that are common
+  for the application, and place them at a single place and reuse them
+  instead of repeating it.
+
+
+  C++ What are Classes and Objects ?
+  -------------------------------------
+
+  Classes and objects are the two main aspects of object-oriented
+  programming.
+
+  Look at the following illustration to see the difference between
+  class and objects:
+
+  Class       |    Objects
+  ------------|-------------
+  Fruits      |     apple
+              |     banana
+              |     mango
+  ------------|-------------
+  Car         |     volvo
+              |     audi
+              |     toyota
+
+
+  So, a class is a template for objects, and an object is an instance
+  of a class.
+
+  When the individual objects are created, they inherit all the variables
+  and functions from the class. */
