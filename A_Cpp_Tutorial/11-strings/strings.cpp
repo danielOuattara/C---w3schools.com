d8891df7e2772adf63f61d0b0e28b5f64d4f5c8e
danielOuattara <daniel.ouattara@gmx.com>
2023-02-13 11:38:57 +0100

    correct text

--- A_Cpp_Tutorial/11-strings/strings.cpp
+++ A_Cpp_Tutorial/11-strings/strings.cpp
@@ -101,8 +101,8 @@ int main(int argc, char const *argv[])
 
     /* You might see some C++ programs that runs without the standard
     namespace library. The using namespace std line can be omitted
-    and replaced with the std keyword, followed by the ::operator
-    for string(and cout) objects : */
+    and replaced with the std keyword, followed by the :: operator
+    for 'string' and 'cout' objects : */
 
     // #include <iostream>
     // #include <string>
